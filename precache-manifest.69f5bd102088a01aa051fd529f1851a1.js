b6cdb6e14047c3bf3fff96636cdb1c326ffff2f3
Miguel Yann <tsambamiguelito@gmail.com>
2020-01-29 06:09:08 +0100

    Updates

---
+++ precache-manifest.69f5bd102088a01aa051fd529f1851a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94b8848410f96bca36edf3d22ca872e",
+    "url": "/SearchMonster/index.html"
+  },
+  {
+    "revision": "78c7faaf57c247e6a4af",
+    "url": "/SearchMonster/static/css/main.f707ab69.chunk.css"
+  },
+  {
+    "revision": "0071328734153483e8c1",
+    "url": "/SearchMonster/static/js/2.1f017594.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/SearchMonster/static/js/2.1f017594.chunk.js.LICENSE"
+  },
+  {
+    "revision": "78c7faaf57c247e6a4af",
+    "url": "/SearchMonster/static/js/main.557a311f.chunk.js"
+  },
+  {
+    "revision": "6fc8f3e1f8b1d6eb61b0",
+    "url": "/SearchMonster/static/js/runtime-main.5c1fe96b.js"
+  }
+]);
